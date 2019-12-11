eeaae52fbf9359bfde7f446428f12f76eeaca25e
= <rwhui.tech@gmail.com>
2019-12-11 22:12:16 +1100

    added header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,38 +2,51 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
-
+// import '../fonts/gotham-cufonfonts/GothamMedium.otf'
 import '../style/index.scss'
+
+const navbarMini = [
+'All Categories',
+'All Stores',
+'Most Popular',
+'New',
+'Women',
+'Men',
+'Kids',
+'Beauty',
+]
 
 const Layout = ({ children, site }) => (
   <div>
     <Helmet title="Snipcart + DatoCMS + GatsbyJS Example" />
     <div className="Container">
       <div className="Header">
-        <div className="Wrap" >
-          <div className="Header__body">
-            <h1 className="Header__title">
-              <Link data-text={site.siteMetadata.siteName} to="/">
-                {site.siteMetadata.siteName}
-              </Link>
-            </h1>
-            <div className="Header__summary snipcart-summary snipcart-checkout">
-              <div className="Header__summary__title">
-                🛍 MY CART 🛍
-              </div>
-              <div className="Header__summary__line">
-                Number of items: <span className="snipcart-total-items"></span>
-              </div>
-              <div className="Header__summary__line">
-                Total price: <span className="snipcart-total-price"></span>
-              </div>
+        <div className="headerContent">
+          <img className="logo" src="https://site-assets.afterpay.com/assets/afterpay_logo-c6f18616342f97c47c82457cf06eb00e0249830809ff092c32727f5dff8a1eba.svg" />
+          <div className="searchBar">
+            <div className="mainSearch">
+              <span>
+                <img/>
+              </span>
+              <input className="searchInput" type="search" placeholder="Search stores and categories"/>
             </div>
+            <button className="searchButton">Search</button>
+          </div>
+          <div className="headerRightSide">
+            <a className="anchorHeaderLink" href="#">How It Works</a>
+            <a className="anchorHeaderLink" href="#">For Retailers</a>
+            <a className="signUp buttonReset grey">Sign Up</a>
+            <a className="logIn buttonReset yellow">Log In</a>
           </div>
         </div>
       </div>
-      <div className="Wrap" >
-        {children}
+      <div className="headerMini">
+        {navbarMini.map(content => <a className="anchorMiniLink" href="#">{content}</a> )}
       </div>
+      
+      {/* This is where contents are loaded for various pages */}
+      {children}
+
       <div className="Wrap" >
         <div className="Footer">
           This website is just an example project to demonstrate how you can 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Layout from "../layouts/index"
 import Img from 'gatsby-image'
+import '../style/home.scss'
 
 export default () => (
   <StaticQuery
@@ -31,6 +32,7 @@ export default () => (
     `}
 render={data => (
   <Layout site={data.site}>
+    <img className="adImg" src="https://site-assets.afterpay.com/pictures/W1siZiIsIjIwMTkvMTEvMjEvMjMvMzEvNDcvZDc2OWExNzMtMWViOC00NzJkLWE3YTQtNGI2YjE1MjRjMWM5LzExXzEzXzIwMTlfSE9NRVBBR0VfMjIwMHg4MDAgKDIpLmpwZyJdLFsicCIsInRodW1iIiwiMTEyMHg-Il0sWyJwIiwiZW5jb2RlIiwianBlZyIsIi1xdWFsaXR5IDg1Il1d/11%2013%202019%20HOMEPAGE%202200x800%20%282%29.jpeg?sha=bdb1ed78ab893fa3"/>
     <div className="Catalogue">
       {
         data.products.edges.map(({ node: product }) => (
