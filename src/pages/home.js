0a209fda28217380cc44457bb4b9c86eb9b11996
= <rwhui.tech@gmail.com>
2019-12-12 10:37:59 +1100

    adding templated section

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,22 +4,42 @@ import Layout from "../layouts/index";
 import Tiles from "../templates/tiles";
 import Mock from "../mockData.js";
 import '../style/home.scss'
+import styled from "styled-components";
+import { Link } from "gatsby";
+import ApSection from "../templates/apSection";
+
+const fakePopular = [
+  {
+    id: 1,
+    src:'https://site-assets.afterpay.com/media/W1siZiIsIjIwMTkvMTIvMDMvMTgvNTYvMjYvMjYxNzA1Y2UtOWM1Yi00ZDU3LWE2NDYtOTkyYmFlY2QwZmU0L0ZpbmlzaExpbmUtMTItMy5qcGciXSxbInAiLCJzdG9yZV90aWxlIiwiMjUweDI1MCJdXQ?sha=7efdb3f731b6a599',
+    merchantName: 'FINISH LINE'
+  },
+  {
+    id: 2,
+    src:'https://site-assets.afterpay.com/media/W1siZiIsIjIwMTkvMTEvMjgvMDUvNDkvMDAvZTYzNGY4MzQtNGE5Zi00MjgxLTlkMzUtYmU2NzdiZGEyYjM3L0NvdHRvbiBPbiBXZWVrIDIyIC0gQWxsIGNhdGVnb3JpZXMgZXhjbHVkaW5nIE1lbnMuanBnIl0sWyJwIiwic3RvcmVfdGlsZSIsIjI1MHgyNTAiXV0?sha=320a3e34bee01e6d',
+    merchantName: 'COTTON ON'
+  },
+  {
+    id: 3,
+    src:'https://site-assets.afterpay.com/media/W1siZiIsIjIwMTgvMTEvMjkvMTcvNDcvMTMvMTE1OWRiOWEtM2QwNS00YTgxLWJkODMtY2QyNTQyYmFmZDliL0NvUG9wX0FmdGVyUGF5XzUwMHg1MDBwaXhfVEZfUjFWMi5qcGciXSxbInAiLCJzdG9yZV90aWxlIiwiMjUweDI1MCJdXQ?sha=a54e50d75f35cb9e',
+    merchantName: 'COLOURPOP'
+  },
+  {
+    id: 4,
+    src:'https://site-assets.afterpay.com/media/W1siZiIsIjIwMTkvMTAvMjkvMjMvMjMvMzYvN2RiMTQ0MzUtZjUxMS00MDI2LWI5MzQtMTU1YTZhNTUzYjhhL0ZvcmV2ZXIyMS5qcGciXSxbInAiLCJzdG9yZV90aWxlIiwiMjUweDI1MCJdXQ?sha=7b840fcf1fa4ecfd',
+    merchantName: 'FOREVER 21'
+  },
+]
 
 const Home = (data) => (
   <div>
     <img className="adImg" src="https://site-assets.afterpay.com/pictures/W1siZiIsIjIwMTkvMTEvMjEvMjMvMzEvNDcvZDc2OWExNzMtMWViOC00NzJkLWE3YTQtNGI2YjE1MjRjMWM5LzExXzEzXzIwMTlfSE9NRVBBR0VfMjIwMHg4MDAgKDIpLmpwZyJdLFsicCIsInRodW1iIiwiMTEyMHg-Il0sWyJwIiwiZW5jb2RlIiwianBlZyIsIi1xdWFsaXR5IDg1Il1d/11%2013%202019%20HOMEPAGE%202200x800%20%282%29.jpeg?sha=bdb1ed78ab893fa3"/>
     {/* <Tiles data={Mock.products} /> */}
-    <div>
-      <div className="title">
-        <span className="mainTitle">Most Popular</span>
-        {/* <a>View All</a> */}
-      </div>
-      <div className="subtitle">Top picks make the best gifts. Shop our most popular retailers.</div>
-      <div className="gallery">
-        {/* <Tiles data={} /> */}
-        
-      </div>
-    </div>
+    <ApSection 
+      title="Most Popular" 
+      subtitle="Top picks make the best gifts. Shop our most popular retailers." 
+      data={fakePopular}
+    />
   </div>
 )
 
---
+++ src/templates/apSection.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import Layout from "../layouts/index";
+import Tiles from "../templates/tiles";
+import Mock from "../mockData.js";
+import '../style/home.scss'
+import styled from "styled-components";
+import { Link } from "gatsby";
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  margin: 16vh 0;
+  justify-content: space-between;
+`;
+
+const Tile = styled.div`
+  width: 220px;
+  height: 220px;
+  margin: 50px 5px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const ApSection = (props) => (
+  <div>
+  <div className="title">
+<span className="mainTitle">{props.title}</span>
+  {/* <a>View All</a> */}
+  </div>
+<div className="subtitle">{props.subtitle}</div>
+  <div className="gallery">
+  <Container className="containers">
+    {console.log(props.data)}
+    {props.data.map(item => (
+      <Tile key={item.id}>
+        <Link to="/blog">
+          <img
+            src={item.src}
+          />
+          <div>
+            <div className="Home_Product__name">{item.merchantName}</div>
+          </div>
+        </Link>
+      </Tile>
+    ))}
+  </Container>
+  </div>
+  </div>
+)
+
+export default ApSection;
